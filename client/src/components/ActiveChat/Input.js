25fd82ab2ae9b43948d27687b4d45147ed1c4fcf
nick <n.wongx@gamil.com>
2022-05-03 10:35:10 -0400

    fix: upload imgs to third party after pressing enter instead of after selecting

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { FormControl, FilledInput } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { SelectImgButton, UnselectImgButton } from './index';
+import axios from "axios";
+import { createFormData, optimizeImgUrl } from '../../utils';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -16,51 +18,88 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
+/**
+ * other Approaches
+ * 1. create a new axios instance (waste for an api)
+ * 2. adding checking to existing interceptors (do not want to modify existing code)
+ */
+axios.interceptors.request.use(async function (config) {
+  delete config.headers['x-access-token'] //preventing cors error
+  return config;
+}, null, {
+  runWhen: (config) => /^https:\/\/api.cloudinary.com/.test(config.url)
+})
 
+const uploadSignedImgs = async (imgs) => {
+  const formDatas = await Promise.all(imgs.map(async (img) => await createFormData(img)))
+
+  return await Promise.all(formDatas.map((formData) => {
+    return axios.post(
+      `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/image/upload`,
+      formData
+    )
+  }))
+}
 
 const Input = ({ otherUser, conversationId, user, postMessage }) => {
   const classes = useStyles();
   const [text, setText] = useState('');
-  const [uploadedImgs, setUploadedImgs] = useState([]);
+  const [selectedImgs, setSelectedImgs] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = (event) => {
+  const memoizedHandleChange = useCallback((event) => {
     setText(event.target.value);
-  };
+  }, [setText]);
 
-  const unselectHandleChange = () => {
-    setUploadedImgs([]);
-  }
+  const memoizedUnselectHandleChange = useCallback(() => {
+    setSelectedImgs([]);
+  }, [setSelectedImgs])
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const form = event.currentTarget;
     const formElements = form.elements;
-    // add sender user info if posting to a brand new convo, so that the other user will have access to username, profile pic, etc.
-    const reqBody = {
-      text: formElements.text.value,
-      recipientId: otherUser.id,
-      conversationId,
-      sender: conversationId ? null : user,
-      attachments: uploadedImgs
-    };
-    await postMessage(reqBody);
-    setText('');
+
+    try {
+      setIsLoading(true);
+      let urls = [];
+      if (selectedImgs.length > 0) {
+        const res = await uploadSignedImgs(selectedImgs);
+        urls = res.map(({ data }) => optimizeImgUrl(data.secure_url));
+      }
+      // add sender user info if posting to a brand new convo, so that the other user will have access to username, profile pic, etc.
+      const reqBody = {
+        text: formElements.text.value,
+        recipientId: otherUser.id,
+        conversationId,
+        sender: conversationId ? null : user,
+        attachments: urls
+      };
+      await postMessage(reqBody);
+    } catch (e) {
+      // error handling
+    } finally {
+      setSelectedImgs([]);
+      setIsLoading(false);
+      setText('');
+    }
   };
 
   return (
     <form className={classes.root} onSubmit={handleSubmit}>
       <FormControl fullWidth hiddenLabel>
         <FilledInput
+          disabled={isLoading}
           classes={{ root: classes.input }}
           disableUnderline
           placeholder="Type something..."
           value={text}
           name="text"
-          onChange={handleChange}
+          onChange={memoizedHandleChange}
           endAdornment={
-            uploadedImgs.length === 0 ?
-            <SelectImgButton onChange={setUploadedImgs}/> :
-            <UnselectImgButton onClick={unselectHandleChange} />
+            selectedImgs.length === 0 ?
+              <SelectImgButton onChange={setSelectedImgs} /> :
+              <UnselectImgButton onClick={memoizedUnselectHandleChange} />
           }
         />
       </FormControl>
--- client/src/components/ActiveChat/SelectImgButton.js
+++ client/src/components/ActiveChat/SelectImgButton.js
@@ -1,68 +1,20 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { IconButton } from "@material-ui/core";
 import PhotoLibraryOutlinedIcon from '@material-ui/icons/PhotoLibraryOutlined';
-import axios from "axios";
-import SnackbarError from '../SnackbarError';
-import { createFormData, optimizeImgUrl } from '../../utils';
-
-/**
- * other Approaches
- * 1. create a new axios instance (waste for an api)
- * 2. adding checking to existing interceptors (do not want to modify existing code)
- */
-axios.interceptors.request.use(async function (config) {
-  delete config.headers['x-access-token'] //preventing cors error
-  return config;
-}, null, {
-  runWhen: (config) => /^https:\/\/api.cloudinary.com/.test(config.url)
-})
-
-
-const uploadSignedImgs = async (imgs) => {
-  const formDatas = await Promise.all(imgs.map(async (img) => await createFormData(img)))
-
-  return await Promise.all(formDatas.map((formData) => {
-    return axios.post(
-      `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/image/upload`,
-      formData
-    )
-  }))
-}
 
 const SelectImgButton = ({ onChange }) => {
   const [imgs, setImgs] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [snackBarOpen, setSnackBarOpen] = useState(false);
 
   const memoizedOnChangeHandler = useCallback((e) => {
     setImgs([...e.target.files]);
   }, [setImgs])
 
   useEffect(() => {
-    const imgsChangeEffect = async () => {
-      if (imgs && imgs.length > 0) {
-        try {
-          const res = await uploadSignedImgs(imgs);
-          const urls = res.map(({ data }) => optimizeImgUrl(data.secure_url));
-          onChange(urls);
-        } catch (e) {
-          setErrorMessage('Something went wrong, please try again');
-          setSnackBarOpen(true);
-        }
-      }
-    }
-    imgsChangeEffect();
+    onChange(imgs);
   }, [imgs, onChange]);
 
   return (
     <label htmlFor="send-img-file">
-      {snackBarOpen && (
-        <SnackbarError
-          setSnackBarOpen={setSnackBarOpen}
-          errorMessage={errorMessage}
-          snackBarOpen={snackBarOpen}
-        />
-      )}
       <input
         accept="image/*"
         id="send-img-file"
